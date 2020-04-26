395dadb502c764b954b16c5435f86de90518ef67
Rudge <rudgee@gmail.com>
2020-04-26 19:45:32 -0300

    Implement scenery of componentTests
    Signed-off-by: Rudge <rudgee@gmail.com>

--- src/componentTest/kotlin/com/rudge/tech/password/PasswordValidationClientTest.kt
+++ src/componentTest/kotlin/com/rudge/tech/password/PasswordValidationClientTest.kt
@@ -1,16 +1,56 @@
 package com.rudge.tech.password
 
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.hasItems
 import org.junit.jupiter.api.Test
 
 class PasswordValidationClientTest {
 
     @Test
     fun `given one string, should call the endpoint password validate and return status code 200 with response password valid`() {
-        TODO("not implemented")
+        Given {
+            port(7000)
+            body("""{"password":"AbTp9!foo"}""")
+        } When {
+            post("/password/validate")
+        } Then {
+            statusCode(200)
+            body("valid", equalTo("true"))
+        }
+    }
+
+    @Test
+    fun `given one string, should call the endpoint password validate and return status code 400 with password not valid`() {
+        Given {
+            port(7000)
+            body("""{"password":"AAAbbbCc"}""")
+        } When {
+            post("/password/validate")
+        } Then {
+            statusCode(200)
+            body("valid", equalTo("false"))
+        }
     }
 
     @Test
     fun `given one string, should call the endpoint password validate and return status code 400 with invalid definitions of password`() {
-        TODO("not implemented")
+        Given {
+            port(7000)
+            body("""{"password":"AAAbbbCc"}""")
+        } When {
+            post("/password/validate")
+        } Then {
+            statusCode(200)
+            body("errors.messages",
+                    hasItems(
+                            "Must have at least 1 number!",
+                            "Must have at least 1 special char!",
+                            "Invalid size, must have more than 9 chars!"
+                    )
+            )
+        }
     }
 }
