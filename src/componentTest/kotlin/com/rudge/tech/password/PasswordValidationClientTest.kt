303f4acfc4e54c2f59d79bd670ea40b1ef07a9bc
Rudge <rudgee@gmail.com>
2020-04-26 13:24:29 -0300

    Add unit and component tests with scenery of success and error.
    Signed-off-by: Rudge <rudgee@gmail.com>

---
+++ src/componentTest/kotlin/com/rudge/tech/password/PasswordValidationClientTest.kt
@@ -0,0 +1,16 @@
+package com.rudge.tech.password
+
+import org.junit.jupiter.api.Test
+
+class PasswordValidationClientTest {
+
+    @Test
+    fun `given one string, should call the endpoint password validate and return status code 200 with response password valid`() {
+
+    }
+
+    @Test
+    fun `given one string, should call the endpoint password validate and return status code 400 with invalid definitions of password`() {
+
+    }
+}
---
+++ src/test/kotlin/com/rudge/tech/password/service/PasswordValidationService.kt
@@ -0,0 +1,17 @@
+package com.rudge.tech.password.service
+
+import org.junit.jupiter.api.Test
+
+class PasswordValidationService {
+
+    @Test
+    fun `given one password should validate this password with success and return true`() {
+
+    }
+
+    @Test
+    fun `given one password should validate this password with error and return false`() {
+
+    }
+
+}
