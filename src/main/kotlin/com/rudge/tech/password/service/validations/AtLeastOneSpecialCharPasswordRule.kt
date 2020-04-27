8853ccba8fbc130a0b89ffb05b5ad1b54363fe47
Rudge <rudgee@gmail.com>
2020-04-26 21:53:53 -0300

    Fix BUG in regex to validate special char.
    Signed-off-by: Rudge <rudgee@gmail.com>

--- src/main/kotlin/com/rudge/tech/password/service/validations/AtLeastOneSpecialCharPasswordRule.kt
+++ src/main/kotlin/com/rudge/tech/password/service/validations/AtLeastOneSpecialCharPasswordRule.kt
@@ -1,5 +1,5 @@
 package com.rudge.tech.password.service.validations
 
 class AtLeastOneSpecialCharPasswordRule(override val invalidMessage: String = "Must have at least 1 special char!") : PasswordRule {
-    override fun test(password: String) = !password.matches("[\\w]".toRegex())
+    override fun test(password: String) = !password.matches("^[\\w&.\\-]+\$".toRegex())
 }
